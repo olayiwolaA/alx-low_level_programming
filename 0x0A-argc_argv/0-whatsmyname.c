86eadf139234602c9469d4ac0398ad284569da8e
olayiwolaA <mike.ow994@gmail.com>
2022-06-24 04:39:07 -0700

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of this program
+ * @argc: length of the arguments
+ * @argv: display the arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+if (argc == 1)
+printf("%s\n", *argv);
+
+return (0);
+}
+
