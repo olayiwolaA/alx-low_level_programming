daa44200b0db5a809b46860a2259092a98e93a19
olayiwolaA <mike.ow994@gmail.com>
2022-06-28 07:18:23 -0700

    README nd main file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_FUNCTIONS
+#define MAIN_FUNCTIONS
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+int _putchar(char c);
+
+#endif
+
