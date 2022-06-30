12d6fb8e170c3d92031e8ee4ceb51962dd4fad72
olayiwolaA <mike.ow994@gmail.com>
2022-06-30 08:56:29 -0700

    Updated Main file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,22 @@
 #ifndef MAIN_FUNCTIONS
 #define MAIN_FUNCTIONS
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+typedef struct add_s
+{
+	char *n_add;
+	int n_dig;
+	int len_r;
+	struct add_s *next;
+} add_t;
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
 
